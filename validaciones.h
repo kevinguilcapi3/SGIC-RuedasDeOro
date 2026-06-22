25bbcc6c1da297d773928de9991863c75c41d7f2
kevinguilcapi3 <kevinguilcapi3@gmail.com>
2026-06-21 21:57:51 -0500

    Create validaciones.h

---
+++ validaciones.h
@@ -0,0 +1,9 @@
+#ifndef VALIDACIONES_H
+#define VALIDACIONES_H
+
+void limpiarBuffer();
+void leerTexto(char texto[], int tam);
+int leerEnteroPositivo(char mensaje[]);
+float leerFloatPositivo(char mensaje[]);
+
+#endif
